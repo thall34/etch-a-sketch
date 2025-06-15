e62a01784b222a4a0c1132c93de6167b1199c58e
Tyler <209157553+thall34@users.noreply.github.com>
2025-06-15 15:30:46 -0400

    Add functionality to existing program
    HTML
    Add button to reset grid
    Add image for etch a sketch logo
    Add div containers to add CSS to recreate toy look
    
    CSS
    Add additional styling to body for fonts
    Add styling for button to set font size and to fit to text content
    Create additional flex containers for sketchpad to mimic real toy
    Create divs for knobs to mimic the look of the toy
    
    Javascript
    Add function to clear grid using querySelectorAll, forEach loop and
    remove function to remove row-items from the container div
    Create function for mouseover event
    Create resetButton function to use the new clearGrid function prompt for
    user input, convert to integer, use an if loop to make sure it is
    between pre-defined range, then if it is in range, use drawGrid function
    using this input and using new sketchpad function after

--- etchSketch.js
+++ etchSketch.js
@@ -1,5 +1,5 @@
 function drawGrid (size) {
-    const gridContainer = document.getElementById("container")
+    const gridContainer = document.getElementById("container");
 
     for (let i = 0; i < size; i++) {
         const rowItem = document.createElement("div");
@@ -13,14 +13,34 @@ function drawGrid (size) {
     };
 };
 
-drawGrid (16);
+function clearGrid () {
+    const rowsRemove = document.querySelectorAll(".row-item");
+    rowsRemove.forEach(row => {
+        row.remove();
+    });
+}
 
-let boxes = document.querySelectorAll(".col-item");
+function sketchpad () {
+    let boxes = document.querySelectorAll(".col-item");
 
     for (let i = 0; i < boxes.length; i++) {
         const box = boxes [i];
 
-        box.addEventListener("mouseover", (e) => {
+        box.addEventListener("mouseover", () => {
         box.className = "col-item-fill";
         });
     };
+}
+
+const resetButton = document.querySelector("#reset")
+    resetButton.addEventListener("click", () => {
+        clearGrid ();
+            let input = prompt("Enter how many squares you want the new grid to be (minimum of 16, maximum of 100)", "");
+            if (input > 15 && input < 101) {
+                let gridNum = parseInt(input);
+                drawGrid (gridNum);
+                sketchpad ();
+            } else {
+                alert("Invalid input, try again");
+            }
+    });
